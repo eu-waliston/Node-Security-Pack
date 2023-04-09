71dda3b803f228150f9c3d1bd263a551a9f0e560
walwizard <goneisgone@hotmail.com>
2023-04-09 11:15:26 -0300

    Node Security Pack - Projeto 2 , Create User

--- Projeto-2/router/root.router.js
+++ Projeto-2/router/root.router.js
@@ -1,50 +1,64 @@
 const express = require("express");
 const User = require("../model/user");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const userRouter = express.Router();
 
-userRouter.get("/register", async (req,res) => {
-    try {
-        //get user input
-        const {firstname, lastName, email, password} = req.body;
+userRouter.get("/register", (req, res) => {
+  res.status(200).send("Register Page");
+});
 
-        //validate user input
-        if(!(email && password && firstname && lastName)) {
-            res.status(400).send("All input is required");
-        }
+userRouter.post("/register", async (req, res) => {
+  try {
+    //get user input
+    const { firstName, lastName, email, password } = req.body;
 
-        //check if user already exist
-        //Validate if user exist in our database
-        const oldUser = await User.findOne({ email });
-
-        if(oldUser) {
-            return res.status(409).send("User Already Exist. Please Login")
-        }
-
-        //Encrypt user password
-        encrypedUsserPasword = await bcrypt.hash(password, 10);
-
-        //creatae user in our database
-        const user = await User.create({
-            first_name: first_name,
-            last_name: last_name,
-            email: email,
-            password: password,
-        })
-
-        //create a token
-        const token = jwt.sign(
-            {user_id: user._id, email},
-            process.env.TOKEN_KEY
-        )
-
-    } catch (err) {
-
+    //validate user input
+    if(!email && !password && !firstName && !lastName) {
+        res.status(400).send("All inputs is required 😠");
     }
-})
 
-userRouter.get("/login", (req,res) => {
-    res.status(200).send("Login Page")
-})
+    //check if user already exist
+    //validate if user exist in our DB
+    const oldUser = await User.findOne({ email });
+    if(oldUser) {
+        return res.status(409).send("User Already Exist 🤔. Please Login😛")
+    }
+
+    //encrypt password
+    const encryptedUserPassword = await bcrypt.hash(password, 10);
+
+    //create user in our DB
+    const user = await User.create({
+        first_name: firstName,
+        last_name: lastName,
+        email: email.toLowerCase(),
+        password: encryptedUserPassword,
+    });
+
+    //create a token
+    const token = jwt.sign(
+        { user_id: user._id, email},
+        process.env.TOKEN_KEY,
+        {
+            expiresIn: "5h"
+        }
+    );
+
+    //save user token
+    user.token = token;
+
+    //return new user
+    res.status(201).json(user);
+
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+userRouter.get("/login", (req, res) => {
+  res.status(200).send("Login Page");
+});
 
 module.exports = userRouter;
