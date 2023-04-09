36dd5d0de710bb33782f78b1567276abf485b188
walwizard <goneisgone@hotmail.com>
2023-04-09 11:41:47 -0300

    Node Security Pack - Projeto 2 , Create User , JWT e Validate

--- Projeto-2/router/root.router.js
+++ Projeto-2/router/root.router.js
@@ -5,25 +5,21 @@ const jwt = require("jsonwebtoken");
 
 const userRouter = express.Router();
 
-userRouter.get("/register", (req, res) => {
-  res.status(200).send("Register Page");
-});
-
 userRouter.post("/register", async (req, res) => {
   try {
     //get user input
     const { firstName, lastName, email, password } = req.body;
 
     //validate user input
-    if(!email && !password && !firstName && !lastName) {
-        res.status(400).send("All inputs is required 😠");
+    if (!email && !password && !firstName && !lastName) {
+      res.status(400).send("All inputs is required 😠");
     }
 
     //check if user already exist
     //validate if user exist in our DB
     const oldUser = await User.findOne({ email });
-    if(oldUser) {
-        return res.status(409).send("User Already Exist 🤔. Please Login😛")
+    if (oldUser) {
+      return res.status(409).send("User Already Exist 🤔. Please Login😛");
     }
 
     //encrypt password
@@ -31,19 +27,19 @@ userRouter.post("/register", async (req, res) => {
 
     //create user in our DB
     const user = await User.create({
-        first_name: firstName,
-        last_name: lastName,
-        email: email.toLowerCase(),
-        password: encryptedUserPassword,
+      first_name: firstName,
+      last_name: lastName,
+      email: email.toLowerCase(),
+      password: encryptedUserPassword,
     });
 
     //create a token
     const token = jwt.sign(
-        { user_id: user._id, email},
-        process.env.TOKEN_KEY,
-        {
-            expiresIn: "5h"
-        }
+      { user_id: user._id, email },
+      process.env.TOKEN_KEY,
+      {
+        expiresIn: "5h",
+      }
     );
 
     //save user token
@@ -51,14 +47,42 @@ userRouter.post("/register", async (req, res) => {
 
     //return new user
     res.status(201).json(user);
-
   } catch (error) {
     console.log(error);
   }
 });
 
-userRouter.get("/login", (req, res) => {
-  res.status(200).send("Login Page");
+userRouter.post("/login", async (req, res) => {
+  try {
+    //get user input
+    const { email, password } = req.body;
+
+    //validate user input
+    if (!email && !password) {
+      res.status(400).send("All input is required 😠");
+    }
+
+    //validate if user exist in our DB
+    const user = await User.findOne({ email });
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      //create a token
+      const token = jwt.sign(
+        { user_id: user._id, email },
+        process.env.TOKEN_KEY,
+        { expiresIn: "5h" }
+      );
+
+      //save user token
+      user.token = token;
+
+      //user
+      return res.status(200).json(user);
+    }
+    return res.status(400).send("Invalid Credencials")
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 module.exports = userRouter;
